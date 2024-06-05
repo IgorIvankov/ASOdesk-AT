0e480514c369db110b58887751d7f65fbed75862
IgorIvankov <127937716+IgorIvankov@users.noreply.github.com>
2024-06-05 11:57:59 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
@@ -7,6 +8,12 @@ module.exports = defineConfig({
   viewportWidth: 1366,
   viewportHeight: 760,
   numTestsKeptInMemory: 0,
+  blockHosts: [
+    '*.yandex.ru*',
+    '*.ads.linkedin.com*',
+    '*.g.doubleclick.net',
+    '*.clarity.*',
+  ],
   video: false,
   retries: 1,
   projectId: 'ASOdesk-AT',
@@ -15,11 +22,18 @@ module.exports = defineConfig({
     configFile: 'reported.config.json',
   },
   videoCompression: 15,
+  env: {
+    MAILSLURP_API_KEY:
+      '4fd4785002b09a41aa1232f2775528a2024128db7eb3927638f6fbd86d7c4c49',
+    RECORD_KEY: '5ef94309-37b4-4f43-81e9-d1a0d79f5add',
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
+    // specPattern: './integration/**/*.{js,jsx,ts,tsx}',
+    baseUrl: 'https://hq.asodesk.com/',
   },
 })
