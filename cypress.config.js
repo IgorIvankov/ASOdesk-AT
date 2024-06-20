b8770a982698981e58a0c8fe95d36002a3890c0e
IgorIvankov <igor_i@asodesk.com>
2024-06-20 11:05:56 +0300

    redux smoke test cancel

--- cypress.config.js
+++ cypress.config.js
@@ -34,7 +34,6 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     specPattern: './cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    baseUrl: 'https://feature-used-redux-toolkit.asodesk.staging.asodesk.com/',
-    // baseUrl: 'https://hq.asodesk.com/',
+    baseUrl: 'https://hq.asodesk.com/',
   },
 })
