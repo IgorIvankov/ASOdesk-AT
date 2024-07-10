5f65d92a49d16332f02386425f8ae1a590459294
IgorIvankov <igor_i@asodesk.com>
2024-07-10 16:34:32 +0300

    Fixed Keyword Boost test 3

--- cypress/e2e/RuStore/UI/KeywordBoost-is-not-available-RS.cy.js
+++ cypress/e2e/RuStore/UI/KeywordBoost-is-not-available-RS.cy.js
@@ -10,16 +10,17 @@ describe('Keyword boost is not available for RuStore apps', () => {
         //Sign in
         auth.signIn('igor_i+rustore@asodesk.com');
 
+        //Choose Russia locale
+        cy.get('div[class="countriesDropdown_dropdown__rV6Yj ml-8 dropdown"]').click();
+        cy.contains('Russia').click();
+
         //Go to rustore app profile page
-        cy.wait(5000);
         cy.get('a[href="/aso-tools/app-profile/ru.tinkoff.sme-rs?locale=ru"]').contains('Т-Бизнес').click();
 
         //Go back to My Apps page with saved rustore app
-        cy.wait(5000);
         cy.get('a[href="/my-apps/?locale=ru"]').contains('My Apps').click();
 
         //Click to Keyword Boost tool to check for redirecting to My Apps page
-        cy.wait(5000);
         cy.get('a[href="/keyword-boost/index/ru.tinkoff.sme-rs?locale=ru"]').contains('Keyword Boost').click();
         cy.url().should('eq', 'https://hq.asodesk.com/my-apps/?locale=ru')
 
