d16efba16b05d8ab4a56a4a73a24f1cc56c50a11
IgorIvankov <igor_i@asodesk.com>
2025-09-08 16:47:32 +0300

    new autotest Localizations

--- cypress/e2e/Classes_library/Constants.js
+++ cypress/e2e/Classes_library/Constants.js
@@ -52,7 +52,7 @@ export class Constants {
     // section for smoke tests
     applications = {
         GP: {'spotify':'com.spotify.music','scanner': 'com.bpmobile.iscanner.free', 'snapchat':'com.snapchat.android', 'pinterest':'com.pinterest', 'vk':'com.vkontakte.android'},
-        AS: {'spotify':'324684580','tiktok': '835599320', 'snapchat':'447188370', 'flirtme':'1336519654', 'onetwotrip': '811057180'},
+        AS: {'spotify':'324684580','tiktok': '835599320', 'snapchat':'447188370', 'flirtme':'1336519654', 'onetwotrip': '811057180', 'pinterest':'429047995'},
         RS: {'tinkoff':'ru.tinkoff.sme-rs'}
     }
 }
---
+++ cypress/e2e/Smoke/API/Tool_is_alive/Localizations-is-alive.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+import {Auth} from "../../../Classes_library/Auth";
+import {Constants} from "../../../Classes_library/Constants";
+
+const constant = new Constants();
+const auth = new Auth();
+const app = constant.applications.AS.pinterest
+
+describe('Localizations should be alive and main requests should response 200', function () {
+    it('Obtain token', function () {
+        auth.obtain();
+    })
+
+    it('app-localization should response 200 and not be empty ', function () {
+        cy.request({
+            method: 'GET',
+            followRedirect: true, log: true, //turn off
+            url: 'api/app-localization/' + app,
+            headers: {
+                'accept': 'application/json',
+                'Authorization': auth.token,
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200)
+                expect(response.body).not.be.empty;
+            })
+    });
+
+});
