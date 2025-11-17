11474f19461a0b06c8e9cbadf1b8f1dd6c8c4ce6
IgorIvankov <igor_i@asodesk.com>
2025-11-17 12:56:02 +0300

    github action. added new extended slack notification

--- cypress/e2e/Demo/API/API-DemoDataTools.js
+++ cypress/e2e/Demo/API/API-DemoDataTools.js
@@ -12,8 +12,7 @@ describe('ASO CR should have Demo data', function () {
         cy.request({
             method: 'GET',
             followRedirect: false, log: true, //turn off
-            //url: '/api/us/301521403/comparative-report-chart?time_since=' + monthAgo + '&time_till=' + toDaysDate,
-            url: '/123/api/us/301521403/comparative-report-chart?time_since=' + monthAgo + '&time_till=' + toDaysDate, //TEST FAILING FOR SLACK NOTIFICATION
+            url: '/api/us/301521403/comparative-report-chart?time_since=' + monthAgo + '&time_till=' + toDaysDate,
             headers: {
                 'accept': 'application/json',
             },
